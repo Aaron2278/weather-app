7ac0b767dafc64a6138874c0c22c3dfacdf57546
Student03 <aaronsantiago542@gmail.com>
2023-10-30 14:55:42 -0400

    progress weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+//const url = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=b1c5b89a28a49878d7596078785708fb`
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Delaware</p>
+          </div>
+          <div className="temp">
+            <h1>68°</h1>
+          </div>
+          <div className="description">
+            <p>Cloudy</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+        </div>
+        <div className="wind">
+          12 MPH
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
 
